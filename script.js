63d12c25aea4f17fa1028cd713e3cdaf53b4d40f
izzy99 <ishanmithun91@gmail.com>
2026-02-09 19:54:52 +0530

    Just start the work

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("hi");
+let userChoice = 0;
+let computerChoice = 0;
+
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+
+
