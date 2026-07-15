bbf7abebb501d6d877c3dd4b5e3105960a989661
Ishan Mithun <ishanmithun91@gmail.com>
2026-07-15 21:58:50 +0530

    Updating

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let computerChoice = "";
 let result = "";
 let playerScore = 0;
 let computerScore = 0;
+let num = 0;
 
 const rockBtn = document.getElementById("rock");
 const paperBtn = document.getElementById("paper");
@@ -23,38 +24,20 @@ scissorBtn.addEventListener("click", function () {
     playGame("scissor");
 });
 
-function playGame(playerPick) {
-
-    const randomNumber = Math.floor(Math.random() * 3) + 1;
-
-    if (randomNumber === 1 ) {
+function playGame(choice) {
+    
+    num = Math.floor(Math.random() * 3);
+    if (num === 0) {
         computerChoice = "rock";
-    } else if (randomNumber === 2 ) {
+    } else if (num === 1) {
         computerChoice = "paper";
     } else {
         computerChoice = "scissor";
     }
 
-    if (computerChoice === userChoice) {
-        result = "It's a DRAW!";
-    } else if ((userChoice === "rock" && computerChoice === "scissor") ||
-        (userChoice === "paper" && computerChoice === "rock") ||
-        (userChoice === "scissor" && computerChoice === "paper")
-    ) {
-        result = "You WIN!";
-        playerScore++;
-
-    }
-
-    pScoreDisplay.innerText = playerScore;
-    cScoreDisplay.innerText = computerScore;
-
-    resultDiv.innerHTML = `
-        <p style="text-transform: uppercase; letter-spacing: 2px; color: #00d4ff;">
-            YOU: ${userChoice} vs CPU: ${computerChoice}
-        </p>
-        <h2 style="font-size: 3rem; text-shadow: 0 0 10px #fff;">${result}</h2>
-    `;
-
 }
 
+playGame();
+
+console.log(num);
+console.log(computerChoice);
