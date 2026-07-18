2d55a25f6da7b7a3ca8c1bac0dfb22f5406e9716
Ishan Mithun <ishanmithun91@gmail.com>
2026-07-18 21:59:38 +0530

    Commit

--- script.js
+++ script.js
@@ -4,17 +4,16 @@ let result = "";
 let playerScore = 0;
 let computerScore = 0;
 let num = 0;
-let playerChoiceSpan = "";
-let computerChoiceSpan = "";
 
-const rockBtn = document.getElementById("rock");
-const paperBtn = document.getElementById("paper");
-const scissorBtn = document.getElementById("scissor");
+
+const rockBtn = document.getElementById("rock-btn");
+const paperBtn = document.getElementById("paper-btn");
+const scissorBtn = document.getElementById("scissor-btn");
+const playerChoiceSpan = document.getElementById("player-choice");
+const computerChoiceSpan = document.getElementById("computer-choice");
 const resultDiv = document.getElementById("result");
 const pScoreDisplay = document.getElementById("player-score");
 const cScoreDisplay = document.getElementById("computer-score");
-const playerChoiceSpan = document.getElementById("player-choice");
-const computerChoiceSpan = document.getElementById("computer-choice");
 
 rockBtn.addEventListener("click", function () {
     playGame("rock");
@@ -29,7 +28,6 @@ scissorBtn.addEventListener("click", function () {
 });
 
 function playGame(choice) {
-
     num = Math.floor(Math.random() * 3);
     if (num === 0) {
         computerChoice = "rock";
@@ -41,7 +39,7 @@ function playGame(choice) {
 
     if (choice === computerChoice) {
         result = "It's a tie!";
-    }else if (
+    } else if (
         (choice === "rock" && computerChoice === "scissor") ||
         (choice === "paper" && computerChoice === "rock") ||
         (choice === "scissor" && computerChoice === "paper")
@@ -56,5 +54,7 @@ function playGame(choice) {
     playerChoiceSpan.textContent = choice;
     computerChoiceSpan.textContent = computerChoice;
     resultDiv.textContent = result;
+    
+    pScoreDisplay.textContent = playerScore;
+    cScoreDisplay.textContent = computerScore;
 }
-
